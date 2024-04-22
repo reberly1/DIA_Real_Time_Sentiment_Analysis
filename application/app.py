0f1c59bf747fab2fcb6458725cb3e569339ecb81
reberly1 <reberly1@umbc.edu>
2024-04-22 09:11:49 -0400

    Initial Flask App Commit
    Basic Flask App with formatting and form inputs.

---
+++ application/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.secret_key = "Dummy Key For Debugging Purposes"
+
+@app.route('/', methods=['GET','POST'])
+def tweet():
+    if request.method == 'POST':
+        tweet = request.form['tweet']
+        return render_template('tweet.html', title="Tweet", tweet=tweet)
+    return render_template('tweet.html', title="Tweet")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return render_template('login.html',title='Login', username=username, password=password)
+    print("Test repo")
+    return render_template('login.html',title='Login')
+
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html',title='Recommended')
