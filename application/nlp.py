7e1dc42288329a1838232486b15fa241a1b67026
reberly1 <reberly1@umbc.edu>
2024-04-26 11:12:34 -0400

    Changed mongodb schema/introduced nlp query

---
+++ application/nlp.py
@@ -0,0 +1,26 @@
+from neo4j import GraphDatabase, basic_auth
+from textblob import TextBlob
+
+def sentiment(tweet):
+    polarity = TextBlob(tweet).sentiment.polarity
+
+    sentiment = ""
+    if polarity < 0:
+        sentiment = "Negative"
+    elif polarity > 0:
+        sentiment = "Positive"
+    else:
+        sentiment = 'Neutral'
+
+    cipher_query = """
+    MATCH (t:Tweet)
+    WHERE t.text = $tweet
+    SET t.sentiment = $sentiment
+    """
+
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "L1r2c3d4!"))
+
+    driver.execute_query(cipher_query, tweet=tweet, sentiment=sentiment, database_="neo4j")
+
+    return
+
