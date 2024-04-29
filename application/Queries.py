e5ed7a18287e6651b578924b460fef9689ffce43
Ali Bowens <abowens1@umbc.edu>
2024-04-28 22:22:12 -0400

    Recommendation and Insertion Queries

--- application/Queries.py
+++ application/Queries.py
@@ -2,24 +2,25 @@ from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "Irunfast3***"))
 #Graph Insertion 
-#Graph Insertion 
-def insert_tweet(username, tweet):   
+def insert_tweet(username, tweet):  
+ with driver.session() as session:
+       append_hashtag = False
        mentioned_users = []
        hashtags = []
-       append_mention = False
-       append_hashtag = False
+       append_mention = False  
+       
 #May have to change to for i in range(len(tweet) - 1): if an out of bounds error occurs
        for i in range(len(tweet)): 
 	#if a space is detected, that means any hashtags or mentions have ended
 	#may have to tweak space check condition
               if tweet[i] == " ":
-                     append_mention = False
+                     append_mention = False 
                      append_hashtag = False
               
               if append_mention == True:
                     mentioned_users[len(mentioned_users) - 1] += tweet[i]
               
-              if append_mention == True:
+              if append_hashtag == True:
                     hashtags[len(hashtags) - 1] += tweet[i]
               
               if tweet[i] == '@':
@@ -28,47 +29,49 @@ def insert_tweet(username, tweet):
               
               if tweet[i] == '#':
                    append_hashtag = True
-                   hashtags.append("")		
+                   hashtags.append("")
 
        cipher_Query = """
-       MATCH (u: User {name: $username})
+       MATCH (u: User {screen_name: $username})
        CREATE (p:Tweet {text: $tweet})
-       CREATE (u)-[:POSTED]->(p)
+       CREATE (u)-[:POSTS]->(p)
        """ 
-       for index, mentioned_user in enumerate(mentioned_users):
+       for  mentioned_user in mentioned_users:
+        if mentioned_user: #skip empty mentions also, makes sure every user is unique
               cipher_Query += f"""
-              MERGE(mu{index}: User {{name: '{mentioned_user}'}})
-              CREATE (p)-[:MENTIONS]->(mu{index})
+              MERGE(mu: User {{screen_name: '{mentioned_user}'}})
+              CREATE (p)-[:MENTIONS]->(mu)
               """
-       for hashtag in hashtags:
+       for hashtag in hashtags: 
+        if hashtag: #skip no hashtags 
               cipher_Query += f"""
               MERGE(h:Hashtag {{name: '{hashtag}'}})
               CREATE (p)-[:CONTAINS]->(h)
               """
+              result = session.run(cipher_Query, username=username,tweet=tweet)
        
        return cipher_Query
 
-
-
+username = "galeister"
+text = "How about this? @28_fireball #data"
+Insertion = insert_tweet(username, text)
+print(Insertion)
 
 def reccomend_tweet(tweeter):
   with driver.session() as session:
     reccomend_query = """
-    MATCH (u:User {name: $username})-[:POSTS]->(:Tweet)-[:CONTAINS]->(h:Hashtag)
-    WITH COLLECT(DISTINCT h.name) AS hashtags
-    MATCH (other:User) -[:POSTS]->(:Tweet)-[:CONTAINS]->(h2:Hashtag)
+    MATCH (u:User {screen_name: $username})-[:POSTS]->(:Tweet)-[:CONTAINS]->(h:Hashtag)
+    WITH u, COLLECT(DISTINCT h.name) AS hashtags
+    MATCH (other:User) -[:POSTS]->(t:Tweet)-[:CONTAINS]->(h2:Hashtag)
     WHERE other <> u AND h2.name IN hashtags 
-    RETURN other.name AS user, tweet.text AS tweet_text
+    RETURN other.name AS user, t.text AS tweet_text
     """
     tweetresult = session.run(reccomend_query, username=tweeter)
     reccomendationsquery = [(record["user"], record["tweet_text"]) for record in tweetresult]
     return reccomendationsquery
 
-
-Username = "Stanley Bishop"
-reccomendations =  reccomend_tweet[Username]
+     
+Username = "BytesByHolly"
+reccomendations =  reccomend_tweet(Username)
 print("These are the reccomendations:", reccomendations)
-     
-     
-
 
